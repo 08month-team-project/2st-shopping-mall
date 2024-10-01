d1cc801aa3270f085a7068c3bca0d609dda7802b
DokyungYou <willow74rr@gmail.com>
2024-10-01 22:22:03 +0900

    ✨Feat(item): findItemAndStockAndSeller 구현
    - 특정 물품 데이터와 해당 물품에 속하는 재고 데이터, 판매자 정보를 한번에 조회

--- src/main/java/com/example/shoppingmall/domain/item/dao/ItemRepository.java
+++ src/main/java/com/example/shoppingmall/domain/item/dao/ItemRepository.java
@@ -4,10 +4,15 @@ import com.example.shoppingmall.domain.item.domain.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     Page<Item> findMainPage(Pageable pageable);
+    @Query("select i from Item i join fetch i.user join fetch i.stocks where i.id = :itemId")
+    Optional<Item> findItemAndStockAndSeller(Long itemId);
 }
