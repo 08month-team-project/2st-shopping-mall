9d9ac06d44300fbed0854d93b04608410e7edb7a
DokyungYou <willow74rr@gmail.com>
2024-10-01 17:18:47 +0900

    ♻️Refactor: Item 엔티티 수정
    - NoArgsConstructor 추가 및 접근레벨 PROTECTED
    - 원시타입 필드  -> wrapper 타입 필드로 수정
    - thumbnailUrl 필드 추가
    - 양방향 관련 주석 추가

--- src/main/java/com/example/shoppingmall/domain/item/domain/Item.java
+++ src/main/java/com/example/shoppingmall/domain/item/domain/Item.java
@@ -3,20 +3,22 @@ package com.example.shoppingmall.domain.item.domain;
 import com.example.shoppingmall.domain.common.BaseTimeEntity;
 import com.example.shoppingmall.domain.user.domain.Users;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.*;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class Item extends BaseTimeEntity {
 
-    // TODO 카테고리
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -36,17 +38,22 @@ public class Item extends BaseTimeEntity {
     @OneToMany(mappedBy = "item")
     private List<ItemImage> images = new ArrayList<>();
 
-    private int stock;
+    @Column(name = "thumbnail_url")
+    private String thumbnailUrl;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "expired_at")
     private LocalDateTime expiredAt;
 
     // TODO 조회 수 구현하게 될 때 생각해볼 예정
-    private long hitCount;
+    @Column(nullable = false, name = "hit_count")
+    private Long hitCount;
 
 
     /* TODO 양방향 고려
-     *  - cate_item
+     *  - cart_item
      *  - order_item
+     *  - item_stock
+     *  - item_category
+     *  - item_image
      */
 }
