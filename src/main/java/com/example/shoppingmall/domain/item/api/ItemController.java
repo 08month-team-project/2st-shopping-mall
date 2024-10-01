4226ee1ded7bfd3b7c2a2a740d4ae8e82fd5a37a
DokyungYou <willow74rr@gmail.com>
2024-10-01 22:30:26 +0900

    ✨Feat(item): 물품 상세정보 조회 기능 구현

--- src/main/java/com/example/shoppingmall/domain/item/api/ItemController.java
+++ src/main/java/com/example/shoppingmall/domain/item/api/ItemController.java
@@ -1,14 +1,12 @@
 package com.example.shoppingmall.domain.item.api;
 
 import com.example.shoppingmall.domain.item.application.ItemService;
+import com.example.shoppingmall.domain.item.dto.ItemDetailResponse;
 import com.example.shoppingmall.domain.item.dto.ItemResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,8 +17,15 @@ public class ItemController {
 
     @GetMapping
     public ResponseEntity<Page<ItemResponse>> getItemMainPage(
-            @RequestParam(name = "page", defaultValue = "1") int pageNum ) {
+            @RequestParam(name = "page", defaultValue = "1") int pageNum) {
 
         return ResponseEntity.ok(itemService.getItemMainPage(pageNum));
+    }
+
+    @GetMapping("/{item_id}")
+    public ResponseEntity<ItemDetailResponse> getItemDetail(
+            @PathVariable("item_id") long itemId) {
+
+        return ResponseEntity.ok(itemService.getItemDetail(itemId));
     }
 }
--- src/main/java/com/example/shoppingmall/domain/item/application/ItemService.java
+++ src/main/java/com/example/shoppingmall/domain/item/application/ItemService.java
@@ -1,24 +1,41 @@
 package com.example.shoppingmall.domain.item.application;
 
+import com.example.shoppingmall.domain.item.dao.ImageRepository;
 import com.example.shoppingmall.domain.item.dao.ItemRepository;
+import com.example.shoppingmall.domain.item.domain.Item;
+import com.example.shoppingmall.domain.item.dto.ItemDetailResponse;
 import com.example.shoppingmall.domain.item.dto.ItemResponse;
+import com.example.shoppingmall.domain.item.excepction.ItemException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import static com.example.shoppingmall.global.exception.ErrorCode.NOT_FOUND_ITEM;
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final ImageRepository imageRepository;
+
     public Page<ItemResponse> getItemMainPage(int pageNum) {
         PageRequest pageRequest = PageRequest.of(pageNum, 12, Sort.by(DESC, "created_at"));
 
         return itemRepository
                 .findMainPage(pageRequest).map(ItemResponse::new);
+    }
+
+    public ItemDetailResponse getItemDetail(long itemId) {
+
+        Item item = itemRepository.findItemAndStockAndSeller(itemId)
+                .orElseThrow(() -> new ItemException(NOT_FOUND_ITEM));
+
+        return new ItemDetailResponse(item, imageRepository.findAllByItemId(item.getId()));
     }
 }
