fa1033e735c8c8219ae36265e08a6e6b67eb36a8
lsy28901 <tndus28901@gmail.com>
2024-10-01 18:48:33 +0900

    🛠Chore(user):Gender Enum @JsonFormat 추가

--- src/main/java/com/example/shoppingmall/domain/user/type/Gender.java
+++ src/main/java/com/example/shoppingmall/domain/user/type/Gender.java
@@ -1,5 +1,8 @@
 package com.example.shoppingmall.domain.user.type;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum Gender {
 
     MALE, FEMALE
