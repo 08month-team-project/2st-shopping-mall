e69babbcced2b338fc83f11d2006867f301df686
lsy28901 <tndus28901@gmail.com>
2024-10-01 19:14:34 +0900

    :sparkles:Feat(user):UserController signup 구현

--- src/main/java/com/example/shoppingmall/domain/user/api/UserController.java
+++ src/main/java/com/example/shoppingmall/domain/user/api/UserController.java
@@ -1,7 +1,27 @@
 package com.example.shoppingmall.domain.user.api;
 
+
+import com.example.shoppingmall.domain.user.application.UserService;
+import com.example.shoppingmall.domain.user.dto.SignupRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Map<String,String>> signup(@Valid @RequestBody SignupRequest signupRequest){
+        Map<String,String> response = userService.createUser(signupRequest);
+        return ResponseEntity.ok(response);
+    }
 }
