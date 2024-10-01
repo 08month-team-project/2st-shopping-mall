6d03ba354bb1b4a0f3cf3a65695b8e4d64fb5aed
DokyungYou <willow74rr@gmail.com>
2024-10-01 14:27:33 +0900

    ✨Feat(item): 물품 목록 조회(일반 조회) 기능 구현
    메인페이지에서 요청하는 기본 조회
    - ItemRepository: findMainPage 추가
    - ItemService: getItemMainPage  추가
    - ItemController: getItemMainPage 추가

--- src/main/java/com/example/shoppingmall/domain/item/api/ItemController.java
+++ src/main/java/com/example/shoppingmall/domain/item/api/ItemController.java
@@ -1,8 +1,26 @@
 package com.example.shoppingmall.domain.item.api;
 
+import com.example.shoppingmall.domain.item.application.ItemService;
+import com.example.shoppingmall.domain.item.dto.ItemResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/items")
 public class ItemController {
 
+    private final ItemService itemService;
+
+    @GetMapping
+    public ResponseEntity<Page<ItemResponse>> getItemMainPage(
+            @RequestParam(name = "page", defaultValue = "1") int pageNum ) {
+
+        return ResponseEntity.ok(itemService.getItemMainPage(pageNum));
+    }
 }
--- src/main/java/com/example/shoppingmall/domain/item/application/ItemService.java
+++ src/main/java/com/example/shoppingmall/domain/item/application/ItemService.java
@@ -1,7 +1,24 @@
 package com.example.shoppingmall.domain.item.application;
 
+import com.example.shoppingmall.domain.item.dao.ItemRepository;
+import com.example.shoppingmall.domain.item.dto.ItemResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
+@RequiredArgsConstructor
 @Service
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+    public Page<ItemResponse> getItemMainPage(int pageNum) {
+        PageRequest pageRequest = PageRequest.of(pageNum, 12, Sort.by(DESC, "created_at"));
+
+        return itemRepository
+                .findAll(pageRequest).map(ItemResponse::new);
+    }
 }
