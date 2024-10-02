4a56985393e6eb39fca816cdd6e95c1ac2f01f28
유종화 <sola201@naver.com>
2024-10-02 21:37:22 +0900

    :sparkles:Feat(seller):ItemController 구현

--- src/main/java/com/example/shoppingmall/domain/item/api/ItemController.java
+++ src/main/java/com/example/shoppingmall/domain/item/api/ItemController.java
@@ -1,8 +1,28 @@
 package com.example.shoppingmall.domain.item.api;
 
+import com.example.shoppingmall.domain.item.application.ItemService;
+import com.example.shoppingmall.domain.item.application.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
+    private final S3Service s3Service;
+
+    // 이미지 업로드
+    @PostMapping("/images/upload")
+    public ResponseEntity<List<String>> getResignedUrls(@RequestParam("images") List<MultipartFile> multipartFiles) {
+        List<String> urls = s3Service.createUrlsForUpload(multipartFiles);
+        return ResponseEntity.ok(urls);
+    }
 
 }
