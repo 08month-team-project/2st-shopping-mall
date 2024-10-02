c8c32a3008d341c5a700d0642b6e427e199f596d
유종화 <sola201@naver.com>
2024-10-02 21:28:36 +0900

    :sparkles:Feat(seller):이미지관련 ErrorCode 작성

--- src/main/java/com/example/shoppingmall/global/exception/ErrorCode.java
+++ src/main/java/com/example/shoppingmall/global/exception/ErrorCode.java
@@ -4,15 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
     temporarily(null, null),
+    INVALID_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "허용되지 않는 파일 형식입니다. JPEG, JPG, PNG 파일만 가능합니다."),
+    IMAGE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "파일 크기가 1MB를 초과합니다."),
     ;
-
 
     private final HttpStatus httpStatus;
     private final String message;
