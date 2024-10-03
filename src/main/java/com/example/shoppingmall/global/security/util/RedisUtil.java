2e9f943c5b3acb63217c9b45202a9d9c68d58f7c
lsy28901 <tndus28901@gmail.com>
2024-10-03 23:15:37 +0900

    :sparkles:Feat(user):redis 핸들링 유틸클래스, 설정파일 추가

---
+++ src/main/java/com/example/shoppingmall/global/security/util/RedisUtil.java
@@ -0,0 +1,23 @@
+package com.example.shoppingmall.global.security.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+    private final RedisTemplate<String,String> redisTemplate;
+
+    public void saveRefreshToken(String email, String refreshToken){
+        redisTemplate.opsForValue().set(email,refreshToken,3, TimeUnit.DAYS);
+    }
+    public String getRefreshToken(String email){
+        return redisTemplate.opsForValue().get(email);
+    }
+    public void deleteRefreshToken(String email){
+        redisTemplate.delete(email);
+    }
+}
