c9874326e9cc0403cac1609e1ac899d7468cc26d
lsy28901 <tndus28901@gmail.com>
2024-10-03 23:11:27 +0900

    :sparkles:Feat(user):에러코드 수정

--- src/main/java/com/example/shoppingmall/global/exception/ErrorCode.java
+++ src/main/java/com/example/shoppingmall/global/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
+    //Auth,UserException
     USER_NOT_FOUND(NOT_FOUND,"사용자를 찾을 수 없습니다."),
     ALREADY_EXIST_USER(CONFLICT,"이미 존재하는 사용자입니다."),
     CREATE_USER_FAILED(INTERNAL_SERVER_ERROR,"회원가입에 실패했습니다."),
