2495cd1036f7f6e0721abb944e550ea69d6ca63a
lsy28901 <tndus28901@gmail.com>
2024-10-03 23:17:46 +0900

    :sparkles:Feat(user):로그아웃 필터 구현

---
+++ src/main/java/com/example/shoppingmall/global/security/filter/CustomLogoutFilter.java
@@ -0,0 +1,99 @@
+package com.example.shoppingmall.global.security.filter;
+
+import com.example.shoppingmall.global.security.util.JwtUtil;
+import com.example.shoppingmall.global.security.util.RedisUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class CustomLogoutFilter extends GenericFilterBean {
+    private final JwtUtil jwtUtil;
+    private final RedisUtil redisUtil;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        doFilter((HttpServletRequest) request,(HttpServletResponse) response, filterChain);
+    }
+    private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException{
+        String requestUri = request.getRequestURI();
+
+        if (!requestUri.matches("^||/users/signout$")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String requestMethod = request.getMethod();
+        if (!requestMethod.equals("POST")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String refreshToken = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("refresh")){
+                refreshToken = cookie.getValue();
+            }
+        }
+
+        if (refreshToken == null){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        try {
+            jwtUtil.isExpired(refreshToken);
+        }catch (ExpiredJwtException e){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String category = jwtUtil.getCategory(refreshToken);
+        if (!category.equals("refresh")){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String email = jwtUtil.getEmail(refreshToken);
+        String redisRefreshToken = redisUtil.getRefreshToken(email);
+        try {
+            if (jwtUtil.isExpired(redisRefreshToken)){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }catch (IllegalArgumentException e){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        if (!compareRefreshToken(refreshToken,redisRefreshToken)){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        Cookie cookie = new Cookie("refresh",null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        redisUtil.deleteRefreshToken(email);
+
+        response.addCookie(cookie);
+        response.setContentType("application/json; charset=UTF-8");
+        response.getWriter().write("{\"message\":\"로그아웃 성공\"}");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private boolean compareRefreshToken(String cookieToken,String redisToken){
+        return cookieToken.equals(redisToken);
+    }
+
+}
