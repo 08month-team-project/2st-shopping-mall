61e4226e0009b8cef3cf99a2bfc5794ea3fc0d6e
유종화 <sola201@naver.com>
2024-10-05 00:20:45 +0900

    :sparkles:Feat(seller): 풀품 등록에 대한 응답 dto 작성

---
+++ src/main/java/com/example/shoppingmall/domain/item/dto/SellerResponse.java
@@ -0,0 +1,12 @@
+package com.example.shoppingmall.domain.item.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SellerResponse {
+
+    private String message;
+
+}
