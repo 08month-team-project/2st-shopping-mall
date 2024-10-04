d7075cf634066273e56fa87862f62e7ef3f3a0f4
lsy28901 <tndus28901@gmail.com>
2024-10-04 16:22:58 +0900

    :sparkles:Feature(user):UserService 이메일 중복 확인 로직 구현

--- src/main/java/com/example/shoppingmall/domain/user/application/UserService.java
+++ src/main/java/com/example/shoppingmall/domain/user/application/UserService.java
@@ -2,8 +2,10 @@ package com.example.shoppingmall.domain.user.application;
 
 import com.example.shoppingmall.domain.user.dao.UserRepository;
 import com.example.shoppingmall.domain.user.domain.User;
+import com.example.shoppingmall.domain.user.dto.CheckEmailRequest;
 import com.example.shoppingmall.domain.user.dto.SignupRequest;
 import com.example.shoppingmall.domain.user.dto.SignupResponse;
+import com.example.shoppingmall.domain.user.dto.UserResponse;
 import com.example.shoppingmall.domain.user.excepction.UserException;
 import com.example.shoppingmall.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -41,4 +43,14 @@ public class UserService {
         }
 
     }
+
+    @Transactional
+    public UserResponse checkEmailDuplicate(CheckEmailRequest checkEmailRequest){
+        if (userRepository.existsByEmail(checkEmailRequest.getEmail())){
+            throw new UserException(ErrorCode.ALREADY_EXIST_USER);
+        }
+        return UserResponse.builder()
+                .message("사용 가능한 이메일입니다.").build();
+    }
+
 }
