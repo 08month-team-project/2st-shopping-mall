165b2ba5c23f136755b81945a53782b292f520f0
유종화 <sola201@naver.com>
2024-10-04 15:51:15 +0900

    :sparkles:Feat(seller): Category 관련 of 스테틱 메서드 추가

--- src/main/java/com/example/shoppingmall/domain/item/dto/CategoryItem.java
+++ src/main/java/com/example/shoppingmall/domain/item/dto/CategoryItem.java
@@ -1,6 +1,7 @@
 package com.example.shoppingmall.domain.item.dto;
 
 import com.example.shoppingmall.domain.item.domain.Category;
+import com.example.shoppingmall.domain.item.type.CategoryName;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,7 +9,7 @@ import lombok.Getter;
 @Builder
 public class CategoryItem {
     
-    private String categoryName;
+    private CategoryName categoryName;
 
     public static CategoryItem fromEntity(Category category) {
         return CategoryItem.builder()
--- src/test/java/com/example/shoppingmall/domain/item/dao/CategoryRepositoryTest.java
+++ src/test/java/com/example/shoppingmall/domain/item/dao/CategoryRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.shoppingmall.domain.item.dao;
 
 import com.example.shoppingmall.domain.item.domain.Category;
+import com.example.shoppingmall.domain.item.type.CategoryName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
+import static com.example.shoppingmall.domain.item.type.CategoryName.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -23,9 +25,9 @@ class CategoryRepositoryTest {
     @DisplayName("카테고리를 저장한 후에 조회하는 테스트")
     void findAllCategoryName() {
         // given
-        Category categoryMan = createCategoryName("남성옷");
-        Category categoryWoman = createCategoryName("여성옷");
-        Category categoryChild = createCategoryName("유아옷");
+        Category categoryMan = createCategoryName(MALE);
+        Category categoryWoman = createCategoryName(FEMALE);
+        Category categoryChild = createCategoryName(CHILDREN);
         categoryRepository.saveAll(List.of(categoryMan, categoryWoman, categoryChild));
 
         // when
@@ -34,12 +36,10 @@ class CategoryRepositoryTest {
         // then
         assertThat(categories).hasSize(3)
                 .extracting("name")
-                .containsExactlyInAnyOrder("남성옷", "여성옷", "유아옷");
+                .containsExactlyInAnyOrder(MALE, FEMALE, CHILDREN);
     }
 
-    private Category createCategoryName(String type) {
-        return Category.builder()
-                .name(type)
-                .build();
+    private Category createCategoryName(CategoryName type) {
+        return Category.of(type);
     }
 }
