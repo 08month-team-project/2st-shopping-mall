9bb7e8b25b193b16f4aefdb312217448bef2804f
DokyungYou <willow74rr@gmail.com>
2024-10-05 18:02:52 +0900

    ✨Feat(item): CartItem 엔티티 생성 메서드 추가 (of), ItemStock 과 연관관계 맺기
    - 요청 수량이 0 이면 1으로 변경
    - 판매가능 상태가 아닌 물품이라면 장바구니 담기 실패
    - 요청 물품의 옵션(사이즈) 의 현재 재고보다 많이 담을 수 없음

--- src/main/java/com/example/shoppingmall/domain/cart/domain/CartItem.java
+++ src/main/java/com/example/shoppingmall/domain/cart/domain/CartItem.java
@@ -2,12 +2,26 @@ package com.example.shoppingmall.domain.cart.domain;
 
 import com.example.shoppingmall.domain.common.BaseTimeEntity;
 import com.example.shoppingmall.domain.item.domain.Item;
+import com.example.shoppingmall.domain.item.domain.ItemStock;
+import com.example.shoppingmall.domain.item.excepction.ItemException;
+import com.example.shoppingmall.domain.item.type.ItemStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+import static com.example.shoppingmall.global.exception.ErrorCode.CART_QUANTITY_EXCEEDS_STOCK;
+import static com.example.shoppingmall.global.exception.ErrorCode.PRODUCT_NOT_FOR_SALE;
 import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.*;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Builder
 @Getter
 @Entity
 public class CartItem extends BaseTimeEntity {
@@ -25,5 +39,33 @@ public class CartItem extends BaseTimeEntity {
     @ManyToOne(fetch = LAZY)
     private Item item;
 
+    @JoinColumn(name = "item_stock_id")
+    @ManyToOne(fetch = LAZY)
+    private ItemStock itemStock;
+
     private int quantity;
+
+    public static CartItem of(Cart cart, Item item, ItemStock itemStock, int quantity) {
+
+        if (quantity == 0) quantity = 1;
+
+        // 물품에 속하는 모든 옵션(사이즈 등) 의 재고가 없을 때 or 만료일자
+        if (item.getStatus() == ItemStatus.OUT_OF_STOCK ||
+                item.getExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new ItemException(PRODUCT_NOT_FOR_SALE);
+        }
+
+        // 현재 재고보다 많은 수량을 담을 수 없음
+        if (itemStock.getStock() < quantity) {
+            throw new ItemException(CART_QUANTITY_EXCEEDS_STOCK);
+        }
+
+        return CartItem.builder()
+                .cart(cart)
+                .item(item)
+                .itemStock(itemStock)
+                .quantity(quantity)
+                .build();
+    }
+
 }
