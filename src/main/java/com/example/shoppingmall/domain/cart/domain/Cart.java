5d03aeedc46a6f0dadee4a34be4ecda0d87b049b
DokyungYou <willow74rr@gmail.com>
2024-10-06 00:15:03 +0900

    ✨Feat(item): Cart 에 양방향 관계 관련 필드, 메서드 추가
    - CartItem 과 양방향 연관관계를 맺었음
    - cartItems 에 CartItem 를 추가하는 메서드 구현

--- src/main/java/com/example/shoppingmall/domain/cart/domain/Cart.java
+++ src/main/java/com/example/shoppingmall/domain/cart/domain/Cart.java
@@ -1,11 +1,15 @@
 package com.example.shoppingmall.domain.cart.domain;
 
+import com.example.shoppingmall.domain.item.domain.ItemStock;
 import com.example.shoppingmall.domain.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -28,12 +32,18 @@ public class Cart {
     @OneToOne(fetch = LAZY)
     private User user;
 
+    @Builder.Default
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cartItems = new ArrayList<>();
 
     public Cart(User user) {
         this.user = user;
     }
 
 
+    public void addCartItem(ItemStock itemStock, int quantity) {
+        cartItems.add(CartItem.of(this, itemStock.getItem(), itemStock, quantity));
+    }
 
 
 }
