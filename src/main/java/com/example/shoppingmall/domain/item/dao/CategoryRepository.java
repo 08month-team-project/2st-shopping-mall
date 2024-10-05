9fc94a8c6e1f5a7f2d1537512fb17273c4e444da
유종화 <sola201@naver.com>
2024-10-05 17:56:04 +0900

    :recycle:Feat(seller): 입력받은 카테고리 조회용 쿼리메서드 작성

--- src/main/java/com/example/shoppingmall/domain/item/dao/CategoryRepository.java
+++ src/main/java/com/example/shoppingmall/domain/item/dao/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.example.shoppingmall.domain.item.dao;
 
 import com.example.shoppingmall.domain.item.domain.Category;
+import com.example.shoppingmall.domain.item.type.CategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+     Optional<Category> findByCategoryName(CategoryName categoryName);
 }
