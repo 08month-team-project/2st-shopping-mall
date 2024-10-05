5a15560a8c0bae3208c7e7a5fd73530b80aecb0b
유종화 <sola201@naver.com>
2024-10-06 01:01:01 +0900

    :sparkles:Feat(seller): 이미지 및 등록 상황 오류코드 추가

--- src/main/java/com/example/shoppingmall/global/exception/ErrorCode.java
+++ src/main/java/com/example/shoppingmall/global/exception/ErrorCode.java
@@ -17,6 +17,10 @@ public enum ErrorCode {
     //ItemException
     NOT_FOUND_ITEM(NOT_FOUND, "물품을 찾을 수 없습니다."),
 
+    // 등록 상황 ItemException
+    NOT_FOUND_CATEGORY(NOT_FOUND,"카테고리를 찾을수 없습니다."),
+    NOT_FOUND_SIZE(NOT_FOUND,"싸이즈를 찾을수 없습니다."),
+
     //S3Exception
     INVALID_IMAGE_TYPE(BAD_REQUEST, "허용되지 않는 파일 형식입니다. JPEG, JPG, PNG 파일만 가능합니다."),
     IMAGE_TOO_LARGE(PAYLOAD_TOO_LARGE, "파일 크기가 1MB를 초과합니다."),
@@ -24,7 +28,8 @@ public enum ErrorCode {
     NOT_VALID_URL(NOT_FOUND,"잘못된 URL 형태입니다."),
     NOT_ENOUGH_IMAGES(NOT_FOUND,"최소 1장의 이미지를 업로드해야 합니다."),
     MAX_UPLOAD_LIMIT(NOT_FOUND,"최대 3장 까지만 업로드가 가능합니다."),
-    INVALID_URL_FORMAT(NOT_FOUND,"잘못된 URL 형식입니다.")
+    INVALID_URL_FORMAT(NOT_FOUND,"잘못된 URL 형식입니다."),
+    UPLOAD_FALL(NOT_FOUND,"업로드에 실패 했습니다.")
 
     ;
 
