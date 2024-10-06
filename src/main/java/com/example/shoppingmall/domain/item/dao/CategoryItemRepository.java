0ff0da7ca3ab7b1499db17a29782fe433aef56b3
유종화 <sola201@naver.com>
2024-10-07 03:27:11 +0900

    :fire: : Feat(seller): DDD 설계중에 필요없다고 판단하여 삭제

--- src/main/java/com/example/shoppingmall/domain/item/dao/CategoryItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shoppingmall.domain.item.dao;
-
-import com.example.shoppingmall.domain.item.domain.CategoryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryItemRepository extends JpaRepository<CategoryItem, Long> {
-}
