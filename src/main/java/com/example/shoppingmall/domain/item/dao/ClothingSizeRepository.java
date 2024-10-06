7adcf78db5145252b15f0342c5fe93f0ab0061dc
유종화 <sola201@naver.com>
2024-10-06 23:19:17 +0900

    Feat(seller): 충돌 해결

--- src/main/java/com/example/shoppingmall/domain/item/dao/ClothingSizeRepository.java
+++ src/main/java/com/example/shoppingmall/domain/item/dao/ClothingSizeRepository.java
@@ -1,9 +1,13 @@
 package com.example.shoppingmall.domain.item.dao;
 
 import com.example.shoppingmall.domain.item.domain.ClothingSize;
+import com.example.shoppingmall.domain.item.type.ClothingSizeName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClothingSizeRepository extends JpaRepository<ClothingSize, Long> {
+    Optional<ClothingSize> findBySizeName(ClothingSizeName SizeName);
 }
