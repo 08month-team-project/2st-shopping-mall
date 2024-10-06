a678276a5bf6ec09c6e1e887b691e018be33de8c
유종화 <sola201@naver.com>
2024-10-06 22:57:29 +0900

    :sparkles:Feat(seller): 삭제 처리된 클래스 저장

--- src/main/java/com/example/shoppingmall/domain/item/dto/ClothingSizeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shoppingmall.domain.item.dto;
-
-import com.example.shoppingmall.domain.item.domain.ClothingSize;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClothingSizeRepository extends JpaRepository<ClothingSize, Long> {
-}
--- src/test/java/com/example/shoppingmall/domain/item/dao/CategoryRepositoryTest.java
+++ src/test/java/com/example/shoppingmall/domain/item/dao/CategoryRepositoryTest.java
@@ -1,12 +1,12 @@
 package com.example.shoppingmall.domain.item.dao;
 
 import com.example.shoppingmall.domain.item.domain.Category;
-import com.example.shoppingmall.domain.item.type.CategoryName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -14,7 +14,8 @@ import static com.example.shoppingmall.domain.item.type.CategoryName.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@DataJpaTest
+@ActiveProfiles("local")
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CategoryRepositoryTest {
 
@@ -25,21 +26,13 @@ class CategoryRepositoryTest {
     @DisplayName("카테고리를 저장한 후에 조회하는 테스트")
     void findAllCategoryName() {
         // given
-        Category categoryMan = createCategoryName(MALE);
-        Category categoryWoman = createCategoryName(FEMALE);
-        Category categoryChild = createCategoryName(CHILDREN);
-        categoryRepository.saveAll(List.of(categoryMan, categoryWoman, categoryChild));
-
+        // 실행 될때마다 모든 카테고리가 등록 되어 있음
         // when
         List<Category> categories = categoryRepository.findAll();
 
         // then
-        assertThat(categories).hasSize(3)
-                .extracting("name")
-                .containsExactlyInAnyOrder(MALE, FEMALE, CHILDREN);
-    }
-
-    private Category createCategoryName(CategoryName type) {
-        return Category.of(type);
+        assertThat(categories).hasSize(4)
+                .extracting("categoryName")
+                .containsExactlyInAnyOrder(MALE,UNISEX ,FEMALE, CHILDREN);
     }
 }
