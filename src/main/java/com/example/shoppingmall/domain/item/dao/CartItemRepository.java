abc94f8a2567b01a88917d99ec1785a0dd38142b
DokyungYou <willow74rr@gmail.com>
2024-10-06 21:02:32 +0900

    ✨Feat(cart): CartItemRepository - findCartItemByFetch 구현
    - cartItemId 를 통해 CartItem 엔티티를 가져온다.
    - 해당 CartItem 이 자신이 담은 물품인지 확인하기 위해 Cart 와 fetch join (cart의 fk인 userId)
    - 현재 판매중인지, 재고가 있는지 확인하기 위해 item , itemStock 과도 fetch join

--- src/main/java/com/example/shoppingmall/domain/item/dao/CartItemRepository.java
+++ src/main/java/com/example/shoppingmall/domain/item/dao/CartItemRepository.java
@@ -17,4 +17,12 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
             " where ci.cart.id = :cartId and ci.itemStock.id = :itemStockId")
     Optional<CartItem> findCartItemByFetch(@Param("cartId") long cartId, @Param("itemStockId") long itemStockId);
 
+
+    @Query("select ci from CartItem" +
+            " ci join fetch ci.cart " +
+            " join fetch ci.item " +
+            " join fetch ci.itemStock " +
+            " where ci.id = :cartItemId")
+    Optional<CartItem> findCartItemByFetch(@Param("cartItemId") long cartItemId);
+
 }
