ab779885353a16c1fc37cc6d7c3ec9e15a8cdfab
TestSeung <sjlim589@hanmail.net>
2024-10-07 11:31:59 +0900

    :sparkles:Feat(my): 게시판조회 api init #25 userEntity comment 추가 조회기능 초기화

--- src/main/java/com/example/shoppingmall/domain/my/api/MyPageController.java
+++ src/main/java/com/example/shoppingmall/domain/my/api/MyPageController.java
@@ -2,11 +2,10 @@ package com.example.shoppingmall.domain.my.api;
 
 import com.example.shoppingmall.domain.my.application.MyPageService;
 import com.example.shoppingmall.domain.my.dto.MyPageRequest;
-import jakarta.validation.Valid;
+import com.example.shoppingmall.global.security.detail.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,8 @@ public class MyPageController {
     private final MyPageService myPageService;
 
     @GetMapping
-    public ResponseEntity<MyPageRequest> myPage(@Valid @AuthenticationPrincipal UserDetails userDetails) {
-        return ResponseEntity.ok(myPageService.check(userDetails));
+    public ResponseEntity<MyPageRequest> myPage(@AuthenticationPrincipal
+                                                    CustomUserDetails userDetails) {
+        return ResponseEntity.ok(myPageService.profileCheck(userDetails));
     }
 }
--- src/main/java/com/example/shoppingmall/domain/my/application/MyPageService.java
+++ src/main/java/com/example/shoppingmall/domain/my/application/MyPageService.java
@@ -3,15 +3,11 @@ package com.example.shoppingmall.domain.my.application;
 
 import com.example.shoppingmall.domain.my.dto.MyPageRequest;
 import com.example.shoppingmall.domain.user.dao.UserRepository;
-import com.example.shoppingmall.domain.user.domain.Address;
 import com.example.shoppingmall.domain.user.domain.User;
 import com.example.shoppingmall.domain.user.excepction.UserException;
-import com.example.shoppingmall.domain.user.type.Gender;
-import com.example.shoppingmall.domain.user.type.UserRole;
-import com.example.shoppingmall.domain.user.type.UserStatus;
 import com.example.shoppingmall.global.exception.ErrorCode;
+import com.example.shoppingmall.global.security.detail.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,11 +16,8 @@ public class MyPageService {
 
     private final UserRepository userRepository;
 
-    public MyPageRequest check(UserDetails userDetails) {
-        //testcode입니다.
-        User user = new User(1L,"exaple@gmail.com","테스트","테스트",
-                "1234567!a","01000000000",new Address(), Gender.MALE, UserRole.CUSTOMER,
-                UserStatus.ACTIVE,null); //? default
+    public MyPageRequest profileCheck(CustomUserDetails userDetails) {
+
         //소개글 추가?
         /* 프론트에서 default 처리?
            따로 db에 저장되지않는 default image를 프론트에서 관리하고
@@ -34,10 +27,7 @@ public class MyPageService {
             db에 image url을 default 처리
             default image 마저 프론트로 전송
          */
-        userRepository.save(user);
-
-
-        User user1=userRepository.findByEmail("example@gmail.com")
+        User user=userRepository.findById(userDetails.getUserId())
                 .orElseThrow(()->new UserException(ErrorCode.USER_NOT_FOUND));
         return new MyPageRequest(user);
     }
