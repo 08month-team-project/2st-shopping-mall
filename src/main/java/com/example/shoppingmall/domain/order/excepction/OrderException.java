42e1e4090be935a8cdc29b7d89b706612d5d33be
DokyungYou <willow74rr@gmail.com>
2024-10-09 19:09:23 +0900

    ✨Feat(order): OrderException 의 필드, 생성자 추가
    - 다른 에러들과는 다르게, 어떤 물품때문에 주문에서 에러가 났는지에 대해 응답으로 보내기 위해 OrderItemResponse 필드 추가
    - 주문 요청 시 OrderCancelOption 의 값이 CANCEL_ENTIRE_ORDER 일 때 한 주문에 대해 문제가 발생 시 요청을 끝내고 예외를 발생시킬 것

--- src/main/java/com/example/shoppingmall/domain/order/excepction/OrderException.java
+++ src/main/java/com/example/shoppingmall/domain/order/excepction/OrderException.java
@@ -1,13 +1,23 @@
 package com.example.shoppingmall.domain.order.excepction;
 
+import com.example.shoppingmall.domain.order.dto.OrderItemResponse;
 import com.example.shoppingmall.global.exception.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public class OrderException extends RuntimeException {
     private final ErrorCode errorCode;
+    private final OrderItemResponse orderItemResponse;
+
     public OrderException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
+        this.orderItemResponse = null;
+    }
+
+    public OrderException(ErrorCode errorCode, OrderItemResponse orderItemResponse) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.orderItemResponse = orderItemResponse;
     }
 }
