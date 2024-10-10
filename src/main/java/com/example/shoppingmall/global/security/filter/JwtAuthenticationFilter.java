bc06478652109573c6e6c155b38ab06cb143780b
TestSeung <sjlim589@hanmail.net>
2024-10-10 17:34:59 +0900

    :sparkles: Feat:AWS 환경 구축 #30 seller

--- src/main/java/com/example/shoppingmall/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/shoppingmall/global/security/filter/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -28,6 +29,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String accessToken = jwtUtil.extractAccessToken(request);
+        log.info(String.valueOf(request));
         String refreshTokenFromCookie = jwtUtil.extractRefreshToken(request.getCookies());
         try {
             if (StringUtils.hasText(accessToken) && !jwtUtil.isExpired(accessToken)) {
